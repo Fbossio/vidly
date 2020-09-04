17c896b5cb03395d33b3fd5810355c24a949b63a
Felix Bossio <felixbossio@gmail.com>
2020-09-04 16:01:33 -0500

    Refactor reusable methods for loginForm into a Form component

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
+import Form from "./common/form";
 import Input from "./common/input";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
     data: { username: "", password: "" },
     errors: {},
@@ -13,43 +14,9 @@ class LoginForm extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.data, this.schema, options);
-
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     // Call the server
     console.log("Submitted");
-  };
-
-  hanldeChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-    const data = { ...this.state.data };
-    data[input.name] = input.value;
-    this.setState({ data, errors });
   };
 
   render() {
@@ -72,9 +39,7 @@ class LoginForm extends Component {
             onChange={this.hanldeChange}
             type="password"
           />
-          <button disabled={this.validate()} className="btn btn-primary">
-            Login
-          </button>
+          {this.renderButton("Loggin")}
         </form>
       </div>
     );
