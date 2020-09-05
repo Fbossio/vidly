faff7d4601ae07db53f2bbb6c4024e4d91823920
Felix Bossio <felixbossio@gmail.com>
2020-09-04 19:00:00 -0500

    modify the MovieForm component

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,5 +1,44 @@
 import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
+class MovieForm extends Form {
+  state = {
+    data: { title: "", genre: "", numInStock: "", rate: "" },
+    errors: {},
+  };
+
+  schema = {
+    title: Joi.string().required().label("Title"),
+    genre: Joi.string().required().label("Genre"),
+    numInStock: Joi.number().integer().min(0).max(100).label("Number in Stock"),
+    rate: Joi.number().min(0).max(10).label("Rate"),
+  };
+
+  doSubmit = () => {
+    // Call the server
+    console.log("Submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Movie Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderInput("genre", "Genre")}
+          {this.renderInput("numInStock", "Number in Stock", "number")}
+          {this.renderInput("rate", "Rate", "number")}
+          {this.renderButton("save")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default MovieForm;
+
+/*
 const MovieForm = ({ match, history }) => {
   return (
     <div>
@@ -15,3 +54,4 @@ const MovieForm = ({ match, history }) => {
 };
 
 export default MovieForm;
+*/
